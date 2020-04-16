9610e3694316d36e2d7cd00ca3b1c900fc4cbb05
luugala <luciagala.barriouevo@hotmail.com>
2020-04-16 20:32:55 -0300

    primera parte del ejercicio

---
+++ app.js
@@ -0,0 +1,21 @@
+// Require de Express
+const express =require ('express');
+
+// Require de FS
+const fs = require ('fs');
+
+// Ejecución de Express
+const app = express ();
+const port = 3030
+// Levantando el Servidor en el puerto 3030
+app.listen(port, () => console.log('Server running in ' + port +'port'));
+
+
+// Ruta Raíz / ➝ Home
+app.get('/', function (req, res){
+	const textoRespuesta = `Ni Superman, Iron Man o La Mujer Maravilla son tan importantes
+	cómo las y los Heroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas 
+	y ellos te sirvan como inspiración para poder cumplir tus objetivos. 
+	Recuerda: ¡nunca pares de creer en ti!.`
+	res.send (textoRespuesta);
+});
