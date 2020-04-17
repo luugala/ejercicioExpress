4d8f0bac4ee8e9b54cc025ff4ea96d0aea83b19a
luugala <luciagala.barriouevo@hotmail.com>
2020-04-16 21:11:01 -0300

    modularizado

--- app.js
+++ app.js
@@ -10,12 +10,4 @@ const port = 3030
 // Levantando el Servidor en el puerto 3030
 app.listen(port, () => console.log('Server running in ' + port +' port'));
 
-
-// Ruta Raíz / ➝ Home
-app.get('/', function (req, res){
-	const textoRespuesta = `Ni Superman, Iron Man o La Mujer Maravilla son tan importantes
-	cómo las y los Heroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas 
-	y ellos te sirvan como inspiración para poder cumplir tus objetivos. 
-	Recuerda: ¡nunca pares de creer en ti!.`
-	res.send (textoRespuesta);
-});
+const home = require ('home')
